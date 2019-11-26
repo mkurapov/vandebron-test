899eb3dd39e909cf3069a4f44422aa54a02723e0
Max Kurapov <maxkurapov@gmail.com>
2019-11-26 20:37:08 +0100

    moving db.json

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://my-json-server.typicode.com/mkurapov/vandebron-test'
     // any config options here
 });
